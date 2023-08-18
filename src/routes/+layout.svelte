2be65723e34bdccdc3d52aad047651c33676d5a3
Tyler <tylergross28@gmail.com>
2023-08-18 14:54:22 -0400

    prevent drag on main websit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,21 @@
 <script lang="ts">
 	import '../app.css';
+
+	function preventDefault(event: DragEvent) {
+		event.preventDefault();
+		event.stopImmediatePropagation();
+	}
 </script>
+
+<style>
+	main {
+		height: 100%;
+		width: 100%;
+	}
+</style>
 
 <div><a href="/">Upload</a> <a href="/books">Books</a></div>
 
-<slot />
+<main draggable="false" on:dragover={preventDefault} on:drop={preventDefault}>
+	<slot />
+</main>
