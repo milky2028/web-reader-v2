65f4b3b9984813700805a5cb218d4efc4d98cb62
Tyler <tylergross28@gmail.com>
2023-08-18 14:54:49 -0400

    drag and drop and main input scaffold set up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,47 @@
 <script lang="ts">
-	function onUpload(event: Event & { currentTarget: HTMLInputElement }) {
-		const files = event.currentTarget?.files ?? [];
+	function getFiles(event: DragEvent | (Event & { currentTarget: HTMLInputElement })) {
+		const files =
+			event instanceof DragEvent ? event.dataTransfer?.files : event.currentTarget.files;
+		return Array.from(files ?? []);
+	}
+
+	function onUpload(event: DragEvent | (Event & { currentTarget: HTMLInputElement })) {
+		const files = getFiles(event);
+
 		for (const file of files) {
 			console.log(file);
 		}
 	}
+
+	function onDragover(event: DragEvent) {
+		event.preventDefault();
+
+		if (event.dataTransfer) {
+			event.dataTransfer.dropEffect = 'copy';
+		}
+	}
 </script>
 
-<input on:change={onUpload} type="file" accept=".cbz, .zip, .cbr, .rar" multiple />
+<style>
+	.container {
+		display: grid;
+		place-items: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.drop-zone {
+		border-radius: 1rem;
+		display: grid;
+		place-items: center;
+		border: 1px solid black;
+		width: 50%;
+		height: 50%;
+	}
+</style>
+
+<div class="container">
+	<div role="button" tabindex="0" on:dragover={onDragover} on:drop={onUpload} class="drop-zone">
+		<input on:change={onUpload} type="file" accept=".cbz, .zip, .cbr, .rar" multiple />
+	</div>
+</div>
