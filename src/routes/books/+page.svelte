b87d0f9a5aa0564217c2b5a1aa529f8439184eee
Tyler <tylergross28@gmail.com>
2023-08-19 11:21:25 -0400

    renderable books page

--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -1,11 +1,24 @@
 <script lang="ts">
 	import { books } from '$lib/stores/books';
+	import { pages } from '$lib/stores/pages';
 </script>
+
+<style>
+	ul {
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+</style>
 
 {#if $books.size > 0}
 	<ul>
-		{#each $books as [bookName]}
-			<li>{bookName}</li>
+		{#each $books as [bookName, book]}
+			<li>
+				<a href="/book/{bookName}/page/0">
+					<img src={pages.getStaticPage(book.coverName)} loading="lazy" alt="" width="200" />
+				</a>
+			</li>
 		{/each}
 	</ul>
 {:else}
