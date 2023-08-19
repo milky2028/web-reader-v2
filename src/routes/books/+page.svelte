347335e32f616404209bf919c7f08ff0c21e3d9f
Tyler <tylergross28@gmail.com>
2023-08-19 10:48:15 -0400

    list book names

--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -1,1 +1,13 @@
-Books
+<script lang="ts">
+	import { books } from '$lib/stores/books';
+</script>
+
+{#if $books.size > 0}
+	<ul>
+		{#each $books as [bookName]}
+			<li>{bookName}</li>
+		{/each}
+	</ul>
+{:else}
+	<p>No books uploaded yet</p>
+{/if}
