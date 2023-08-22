5ad834f6e93cbb1035d00195b975e8c53042a6e0
Tyler <tylergross28@gmail.com>
2023-08-22 19:32:58 -0400

    simplify

--- src/lib/filesystem/writeManifest.ts
+++ src/lib/filesystem/writeManifest.ts
@@ -4,7 +4,7 @@ import { writeFile } from './writeFile';
 export const MANIFEST_NAME = 'book-manifest.json';
 
 export function writeManifest(manifest: Map<string, BookDetails>) {
-	const entries = Array.from(manifest.entries());
+	const entries = Array.from(manifest);
 	const file = new File([JSON.stringify(entries)], MANIFEST_NAME, { type: 'application/json' });
 	return writeFile(`/${MANIFEST_NAME}`, file);
 }
