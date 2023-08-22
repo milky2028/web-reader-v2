34121b6b18854e48d84e2c0e6fc635e1a71ea6e7
Tyler <tylergross28@gmail.com>
2023-08-22 19:51:45 -0400

    catch errors if manifest does not exist

--- src/lib/filesystem/getManifest.ts
+++ src/lib/filesystem/getManifest.ts
@@ -3,9 +3,12 @@ import { getFile } from './getFile';
 import { MANIFEST_NAME } from './writeManifest';
 
 export async function getManifest() {
-	const file = await getFile(`/${MANIFEST_NAME}`);
-	const fileContents = await file.text();
-	const json: [string, BookDetails][] = JSON.parse(fileContents);
-
-	return new Map(json);
+	try {
+		const file = await getFile(`/${MANIFEST_NAME}`);
+		const fileContents = await file.text();
+		const json: [string, BookDetails][] = JSON.parse(fileContents);
+		return new Map(json);
+	} catch {
+		return new Map();
+	}
 }
