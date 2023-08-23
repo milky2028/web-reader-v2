a26147f19ff98c27ac4cbb82ba35529211ef8650
Tyler <tylergross28@gmail.com>
2023-08-22 20:03:45 -0400

    load books in parallel

--- src/routes/books/+page.svelte
+++ src/routes/books/+page.svelte
@@ -4,10 +4,12 @@
 	import { derived } from 'svelte/store';
 
 	const covers = derived([books, pages], ([$books, _$pages]) =>
-		[...$books].map(async ([bookName, book]) => ({
-			bookName,
-			coverImg: await pages.getPage($books, bookName, book.coverName)
-		}))
+		Promise.all(
+			[...$books].map(async ([bookName, book]) => ({
+				bookName,
+				coverImg: await pages.getPage($books, bookName, book.coverName)
+			}))
+		)
 	);
 </script>
 
@@ -27,20 +29,20 @@
 	}
 </style>
 
-{#if $books.size > 0}
-	<ul>
-		{#each $covers as cover}
-			{#await cover}
-				<li>Loading...</li>
-			{:then { bookName, coverImg }}
+{#await $covers}
+	<div>Loading...</div>
+{:then cover}
+	{#if $books.size === 0}
+		<p>No books uploaded yet</p>
+	{:else}
+		<ul>
+			{#each cover as { bookName, coverImg } (coverImg)}
 				<li>
 					<a href="/book/{bookName}/page/0">
 						<img src={coverImg} loading="lazy" alt={bookName} width="200" />
 					</a>
 				</li>
-			{/await}
-		{/each}
-	</ul>
-{:else}
-	<p>No books uploaded yet</p>
-{/if}
+			{/each}
+		</ul>
+	{/if}
+{/await}
