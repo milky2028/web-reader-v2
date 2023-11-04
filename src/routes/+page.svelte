bca8a72a91e51a408992bbfcb00eab8e16ccb5ff
Tyler Gross <tylergross28@gmail.com>
2023-11-04 13:38:59 -0400

    prevent drag and drop of disallowed files

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,7 +20,7 @@
 	function getFiles(event: DragEvent | (Event & { currentTarget: HTMLInputElement })) {
 		const files =
 			event instanceof DragEvent ? event.dataTransfer?.files : event.currentTarget.files;
-		return Array.from(files ?? []);
+		return Array.from(files ?? []).filter((file) => file.name.match(/.cbr|.cbz|.rar|.zip$/));
 	}
 
 	async function onUpload(event: DragEvent | (Event & { currentTarget: HTMLInputElement })) {
@@ -86,7 +86,7 @@
 		const [bookName] = await Promise.all(extractFiles);
 		if (files.length === 1) {
 			goto(`/book/${bookName}/page/0`);
-		} else {
+		} else if (files.length > 0) {
 			goto('/books');
 		}
 	}
@@ -94,6 +94,7 @@
 
 <style>
 	.container {
+		position: absolute;
 		display: grid;
 		place-items: center;
 		width: 100%;
