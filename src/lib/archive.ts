d7d6bad215caaa432ccc544dedb96818436f6010
Tyler <tylergross28@gmail.com>
2023-12-31 09:32:57 -0500

    serve worker from isolation

--- src/lib/archive.ts
+++ src/lib/archive.ts
@@ -1,7 +1,8 @@
 import { Archive as libarchive } from 'libarchive.js';
 import { CompressedFile as libarchiveCompressedFile } from 'libarchive.js/src/compressed-file';
+import workerUrl from '../../node_modules/libarchive.js/dist/worker-bundle.js?url';
 
-libarchive.init({ workerUrl: '/worker-bundle.js' });
+libarchive.init({ workerUrl });
 
 export type FilesArrayObject = { file: File | libarchiveCompressedFile; path: string };
 export const CompressedFile = libarchiveCompressedFile;
