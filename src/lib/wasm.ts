750807fc4cfcd0773100e68d961119358f3ca7f1
Tyler <tylergross28@gmail.com>
2024-01-01 14:17:14 -0500

    use dev urls

--- src/lib/wasm.ts
+++ src/lib/wasm.ts
@@ -1,8 +1,8 @@
 import type init from 'extract-zip-rar';
-import { dev } from '$app/environment';
+// import { dev } from '$app/environment';
 
-import moduleUrl from '../../node_modules/extract-zip-rar/dist/extract.js?url';
-import wasmURL from '../../node_modules/extract-zip-rar/dist/extract.wasm?url';
+// import moduleUrl from '../../node_modules/extract-zip-rar/dist/extract.js?url';
+// import wasmURL from '../../node_modules/extract-zip-rar/dist/extract.wasm?url';
 
 import devModuleUrl from '../../node_modules/extract-zip-rar/dist/extract.debug.js?url';
 import devWasmUrl from '../../node_modules/extract-zip-rar/dist/extract.debug.wasm?url';
@@ -12,8 +12,8 @@ declare global {
 	var wasmURL: string;
 }
 
-globalThis.wasmURL = dev ? devWasmUrl : wasmURL;
-const { default: initialize } = await import(dev ? devModuleUrl : moduleUrl);
+globalThis.wasmURL = devWasmUrl;
+const { default: initialize } = await import(devModuleUrl);
 const _initialize = initialize as typeof init;
 
 export const wasm = await _initialize();
