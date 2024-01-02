d73e010b229c8d8ac2e2374385a8dcb7af5c8779
Tyler <tylergross28@gmail.com>
2024-01-02 17:49:28 -0500

    fix page extraction issue

--- src/lib/workers/extractionWorker.ts
+++ src/lib/workers/extractionWorker.ts
@@ -24,14 +24,12 @@ self.addEventListener(
 
 		// handle cases where cover is not the first file in the archive
 		const cover = entry_iterator.next().value;
-		const initialPages = range({ start: 1, end: Math.min(12, pages.length) }).map(
-			async (pageNumber) => {
-				const page = entry_iterator.next().value;
-				if (page?.file) {
-					await writeFile(`/books/${bookName}/${pages[pageNumber]}`, page.file);
-				}
+		const initialPages = range({ start: 1, end: Math.min(12, pages.length) }).map(async () => {
+			const page = entry_iterator.next().value;
+			if (page?.file) {
+				await writeFile(`/books/${bookName}/${page.fileName}`, page.file);
 			}
-		);
+		});
 		await Promise.all(initialPages);
 
 		if (cover?.file) {
