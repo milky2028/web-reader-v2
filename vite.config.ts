0c5cdb67ac0ef5e05c3345a5b053b5e76ba30ffd
Tyler <tylergross28@gmail.com>
2024-01-12 20:53:54 -0500

    let sigh

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,32 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
+import cert from 'vite-plugin-mkcert';
 
-export default async ({ mode }: { mode: 'production' }) => {
-	const devPlugins =
-		mode !== 'production'
-			? [
-					await import('vite-plugin-mkcert').then((m) => m.default),
-					{
-						name: 'configure-response-headers',
-						configureServer: (server: any) => {
-							server.middlewares.use((_req: any, res: any, next: any) => {
-								res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-								res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+export default defineConfig(({ mode }) => ({
+	plugins: [
+		sveltekit(),
+		cert(),
+		{
+			name: 'configure-response-headers',
+			configureServer: (server) => {
+				server.middlewares.use((_req, res, next) => {
+					res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+					res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
 
-								next();
-							});
-						}
-					}
-			  ]
-			: [];
-
-	return {
-		plugins: [sveltekit(), ...devPlugins],
-		define: {
-			__IS_DEV__: mode !== 'production'
-		},
-		worker: {
-			format: 'es'
-		},
-		build: { target: 'esnext' },
-		server: { https: true },
-		test: {
-			include: ['src/**/*.{test,spec}.{js,ts}']
+					next();
+				});
+			}
 		}
-	};
-};
+	],
+	define: {
+		__IS_DEV__: mode !== 'production'
+	},
+	worker: {
+		format: 'es'
+	},
+	build: { target: 'esnext' },
+	server: { https: true },
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	}
+}));
