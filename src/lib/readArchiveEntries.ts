7338d293feae81dfc3026ccd7d14832be65270dd
Tyler Gross <tylergross28@gmail.com>
2025-05-04 22:39:48 -0400

    disable exceptions

--- src/lib/readArchiveEntries.ts
+++ src/lib/readArchiveEntries.ts
@@ -12,7 +12,6 @@ function isImage(path: string) {
 
 export function* readArchiveEntries({ file, wasm, extractData = false }: ReadArchiveEntriesParams) {
 	const {
-		getExceptionMessage,
 		open_archive,
 		close_archive,
 		get_next_entry,
@@ -26,39 +25,31 @@ export function* readArchiveEntries({ file, wasm, extractData = false }: ReadArc
 		ENTRY_ERROR
 	} = wasm;
 
-	try {
-		const archivePtr = open_archive(file.ptr, file.size);
+	const archivePtr = open_archive(file.ptr, file.size);
 
-		for (;;) {
-			const entryPtr = get_next_entry(archivePtr);
-			if (entryPtr === END_OF_FILE || entryPtr === ENTRY_ERROR) {
-				close_archive(archivePtr);
-				return;
-			}
-
-			const path = get_entry_name(entryPtr).toLowerCase();
-			const isFile = entry_is_file(entryPtr);
-
-			if (isFile && !path.startsWith('__macosx') && isImage(path)) {
-				const fileName = path.split('/').pop() ?? '';
-				if (extractData) {
-					const size = get_entry_size(entryPtr);
-					const entry_data = read_entry_data(archivePtr, entryPtr);
-					const buffer = get_buffer(entry_data, size);
-					const file = new File([buffer.slice()], fileName, { type: 'application/octet-stream' });
-					free_buffer(entry_data);
-
-					yield { fileName, file };
-				} else {
-					yield { fileName };
-				}
-			}
-		}
-	} catch (error) {
-		if (error instanceof WebAssembly.Exception) {
-			throw getExceptionMessage(error)[1];
+	for (;;) {
+		const entryPtr = get_next_entry(archivePtr);
+		if (entryPtr === END_OF_FILE || entryPtr === ENTRY_ERROR) {
+			close_archive(archivePtr);
+			return;
 		}
 
-		throw error;
+		const path = get_entry_name(entryPtr).toLowerCase();
+		const isFile = entry_is_file(entryPtr);
+
+		if (isFile && !path.startsWith('__macosx') && isImage(path)) {
+			const fileName = path.split('/').pop() ?? '';
+			if (extractData) {
+				const size = get_entry_size(entryPtr);
+				const entry_data = read_entry_data(archivePtr, entryPtr);
+				const buffer = get_buffer(entry_data, size);
+				const file = new File([buffer.slice()], fileName, { type: 'application/octet-stream' });
+				free_buffer(entry_data);
+
+				yield { fileName, file };
+			} else {
+				yield { fileName };
+			}
+		}
 	}
 }
